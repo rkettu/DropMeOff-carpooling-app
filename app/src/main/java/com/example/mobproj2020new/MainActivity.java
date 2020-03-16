0d0172be2d519b75c37f3222c246a0b2a2ad5e00
Saku Jamsa <t8jasa01@students.oamk.fi>
2020-03-16 15:01:25 +0200

    testi

--- app/src/main/java/com/example/mobproj2020new/MainActivity.java
+++ app/src/main/java/com/example/mobproj2020new/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.mobproj2020new;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.startButton).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent maps_testi = new Intent(this, MapsActivity.class);
+        startActivity(maps_testi);
     }
 }
--- app/src/main/java/com/example/mobproj2020new/MapsActivity.java
+++ app/src/main/java/com/example/mobproj2020new/MapsActivity.java
@@ -1,32 +1,41 @@
 package com.example.mobproj2020new;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private int moi = 2;
-    private int moi2 = 3;
+    //FusedLocationProviderClient mFusedLocationClient;
+    //private Double latitude = 0.0;
+    //private Double longitude = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
     }
-
 
     /**
      * Manipulates the map once available.
@@ -41,9 +50,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng oulu = new LatLng(65.04, 25.56);
+        mMap.addMarker(new MarkerOptions().position(oulu).title("Marker in Oulu"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(oulu));
     }
 }
