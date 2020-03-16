fd6ef5ae5334e231e7425bcabe15b84738fc1e21
Roope <roope-kettu@hotmail.com>
2020-03-16 10:50:12 +0200

    gitignore test

--- app/src/main/java/com/example/mobproj2020new/MapsActivity.java
+++ app/src/main/java/com/example/mobproj2020new/MapsActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private int moi = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
