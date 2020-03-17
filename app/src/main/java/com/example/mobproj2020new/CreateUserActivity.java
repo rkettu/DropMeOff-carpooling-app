ac33dcc053777ce6bce16dd4463a69a280ec3dd3
Roope <roope-kettu@hotmail.com>
2020-03-17 15:02:27 +0200

    Added simple Firebase login system. todo: store phone number and other user data

---
+++ app/src/main/java/com/example/mobproj2020new/CreateUserActivity.java
@@ -0,0 +1,120 @@
+package com.example.mobproj2020new;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class CreateUserActivity extends AppCompatActivity {
+    private final String TAG = "CREATEUSERACTIVITY";
+
+    private EditText fnameField;
+    private EditText lnameField;
+    private EditText emailField;
+    private EditText phoneField;
+    private EditText pass1Field;
+    private EditText pass2Field;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        fnameField = (EditText) findViewById(R.id.fnameField);
+        lnameField = (EditText) findViewById(R.id.lnameField);
+        emailField = (EditText) findViewById(R.id.userEmailField);
+        phoneField = (EditText) findViewById(R.id.phoneField);
+        pass1Field = (EditText) findViewById(R.id.passwordField1);
+        pass2Field = (EditText) findViewById(R.id.passwordField2);
+
+        pass2Field.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Check if password fields match
+                if(pass1Field.getText().toString().equals(pass2Field.getText().toString())) {
+                    // Passwords match!
+                }
+                else {
+                    // Passwords don't match
+                    // Update some textview in realtime that says passwords don't match
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        final Button createUserBtn = (Button) findViewById(R.id.createUserBtn);
+        createUserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateUserBtnFunc();
+            }
+        });
+    }
+
+    private void CreateUserBtnFunc()
+    {
+        String email = emailField.getText().toString();
+        String pass1 = pass1Field.getText().toString();
+        String pass2 = pass2Field.getText().toString();
+        String phone = phoneField.getText().toString();         // CURRENTLY UNUSED
+        final String dname = fnameField.getText().toString() + " " + lnameField.getText().toString(); // SAME
+
+        if(email.equals("") || pass1.equals("") || phone.equals("") || dname.equals(""))
+        {
+            Toast.makeText(CreateUserActivity.this, "Some fields empty",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else if(!pass1.equals(pass2)) {
+            Toast.makeText(CreateUserActivity.this, "Passwords don't match",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else {
+            mAuth.createUserWithEmailAndPassword(email, pass1).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(dname).build();
+                        user.updateProfile(profileUpdates);
+
+                    } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(CreateUserActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        //  Sign in failed...
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mobproj2020new/LoggedInActivity.java
@@ -0,0 +1,46 @@
+package com.example.mobproj2020new;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
+public class LoggedInActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_in);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        final Button signOutBtn = (Button) findViewById(R.id.signOutBtn);
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                ReturnToLogin();
+            }
+        });
+
+        TextView userInfo = (TextView)findViewById(R.id.userInfoTextView);
+        userInfo.setText(mUser.getDisplayName()+"\n"+mUser.getEmail()+"\n"+mUser.getPhoneNumber());
+    }
+
+    private void ReturnToLogin()
+    {
+        // Currently returns only to previous...
+        this.finish();
+    }
+}
