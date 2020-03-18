9d0bd50f12afbf1f9de3ccebe9a660e59e2cba8c
JaniKorteniemi <jkorteniemi@gmail.com>
2020-03-18 11:18:17 +0200

    Login and signup 1.0 first Commit

--- app/src/main/java/com/example/mobproj2020new/MainActivity.java
+++ app/src/main/java/com/example/mobproj2020new/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.mobproj2020new;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText userEdit;
+    EditText passEdit;
+
+    String userNameStr = "";
+    String passwordStr = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userEdit = findViewById(R.id.usernameEdit);
+        passEdit = findViewById(R.id.passwordEdit);
+    }
+
+    //Login button press
+    public void login(View V){
+        if (userEdit != null && userEdit.length() > 0){
+
+        }else {
+            noEntry(userEdit);
+        }
+        if(passEdit != null && passEdit.length() > 0){
+
+        }else {
+            noEntry(passEdit);
+        }
+    }
+
+    //SignUp button press
+    public void signUp(View v){
+        Intent signUpIntent = new Intent(this, SignUp.class);
+        startActivity(signUpIntent);
+    }
+
+    //execute if username or password is empty
+    public void noEntry(EditText et){
+        et.setText("");
+        et.setHintTextColor(Color.parseColor("#B75252"));
+        if (et.getId() == R.id.usernameEdit) userEdit.setHint("Username*");
+        else passEdit.setHint("Password*");
+    }
+
+    //Exit app with pressing back putton on your phone
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        return;
     }
 }
