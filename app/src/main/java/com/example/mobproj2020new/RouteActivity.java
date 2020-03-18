3ce3b1e4c385d1a68328ddc35ec984911449f587
Saku Jamsa <t8jasa01@students.oamk.fi>
2020-03-18 16:12:08 +0200

    reittiominaisuus lisätty, vielä hiomista

--- app/src/main/java/com/example/mobproj2020new/RouteActivity.java
+++ app/src/main/java/com/example/mobproj2020new/RouteActivity.java
@@ -6,12 +6,17 @@ import androidx.loader.content.AsyncTaskLoader;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,17 +26,19 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 
-public class RouteActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback{
+public class RouteActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback, View.OnClickListener{
 
     private GoogleMap mMap;
     MarkerOptions place1, place2;
     Polyline currentPolyline;
-    Button haeReitti;
+    private FusedLocationProviderClient fusedLocationClient;
 
 
     @Override
@@ -43,34 +50,88 @@ public class RouteActivity extends AppCompatActivity implements OnMapReadyCallba
                 .findFragmentById(R.id.map2);
         mapFragment.getMapAsync(this);
 
-        haeReitti = findViewById(R.id.haeButton);
-        haeReitti.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText lahtoEditori = (EditText) findViewById(R.id.lahtoEdit);
-                String strLahto = lahtoEditori.getText().toString();
-                Double lahtoPiste = Double.parseDouble(strLahto);
-                if (strLahto.trim().equals(""))
-                {
-                    Toast.makeText(getApplicationContext(),"Valitse lahtö- tai loppupiste", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    place1 = new MarkerOptions().position(new LatLng(lahtoPiste, 25.478431)).title("Location 1");
-                    place2 = new MarkerOptions().position(new LatLng(63.68855, 25.978137)).title("Location 2");
-                    new FetchURL(RouteActivity.this).execute(getUrl(place1.getPosition(), place2.getPosition(), "driving"), "driving");
-                    mMap.addMarker(place1);
-                    mMap.addMarker(place2);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        //final Geocoder geocoder2 = new Geocoder(this);
 
-                }
-
-            }
-        });
-
-        //origin=65.027,25.478&destination=63.688,25.978
-        //String url = getUrl(place1.getPosition(), place2.getPosition(), "driving");
+        findViewById(R.id.haeButton).setOnClickListener(this);
+        findViewById(R.id.sijaintiButton).setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.haeButton)
+        {
+            SearchView lahtoEditori = (SearchView) findViewById(R.id.lahtoEdit);
+            SearchView loppuEditori = (SearchView) findViewById(R.id.maaranpaaEdit);
+
+            String strLahto = lahtoEditori.getQuery().toString();
+            String strLoppu = loppuEditori.getQuery().toString();
+
+            if (strLahto.trim().equals("") || strLoppu.trim().equals(""))
+            {
+                Toast.makeText(getApplicationContext(),"Valitse lahtö- ja loppupiste", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                try {
+                    geoLocate(strLahto, strLoppu);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        else if (v.getId() == R.id.sijaintiButton)
+        {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if(location != null){
+                                String latitude = String.valueOf(location.getLatitude());
+                                String longitude = String.valueOf(location.getLongitude());
+                                Toast.makeText(getApplicationContext(),latitude + "," + longitude, Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(),"Ei sijaintia", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    public void geoLocate(String start, String stop) throws IOException {
+        mMap.clear();
+
+        Geocoder gc = new Geocoder(this);
+
+        List<Address> listStart = gc.getFromLocationName(start, 1);
+        Address add = listStart.get(0);
+
+        List<Address> listStop = gc.getFromLocationName(stop, 1);
+        Address add2 = listStop.get(0);
+
+        double startLat = add.getLatitude();
+        double startLon = add.getLongitude();
+
+        double stopLat = add2.getLatitude();
+        double stopLon = add2.getLongitude();
+
+        place1 = new MarkerOptions().position(new LatLng(startLat, startLon)).title("Location 1");
+        place2 = new MarkerOptions().position(new LatLng(stopLat, stopLon)).title("Location 2");
+
+        new FetchURL(RouteActivity.this).execute(getUrl(place1.getPosition(), place2.getPosition(), "driving"), "driving");
+
+        mMap.addMarker(place1);
+        mMap.addMarker(place2);
+
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(place1.getPosition()));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place1.getPosition(),10));
+
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -105,4 +166,5 @@ public class RouteActivity extends AppCompatActivity implements OnMapReadyCallba
             currentPolyline.remove();
         currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
     }
+
 }
