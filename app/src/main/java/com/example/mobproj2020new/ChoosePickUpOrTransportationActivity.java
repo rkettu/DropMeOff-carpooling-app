0f2d7000ae8183ac06650232c3fb47ed3507e1a2
JanneL-oamk <lampela.janne1@gmail.com>
2020-03-19 13:38:04 +0200

    Added settings popup dialog

--- app/src/main/java/com/example/mobproj2020new/ChoosePickUpOrTransportationActivity.java
+++ app/src/main/java/com/example/mobproj2020new/ChoosePickUpOrTransportationActivity.java
@@ -1,14 +1,101 @@
 package com.example.mobproj2020new;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ChoosePickUpOrTransportationActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.nio.channels.AlreadyBoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChoosePickUpOrTransportationActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_pick_up_or_transportation);
     }
+
+    //-----------Applications settings button------------//
+    public void AppSettings(View v) {
+        final String[] itemList = {"Settings", "About", "Sign Out"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setItems(itemList, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case 0:
+                        //Log.d("SWAG", "onClick: settings");
+                        break;
+                    case 1:
+                        //Log.d("SWAG", "onClick: About");
+                        break;
+                    case 2:
+                        //Log.d("SWAG", "onClick: Sign Out");
+                        onBackPressed();
+                        break;
+                }
+            }
+        });
+        AlertDialog settingsDialog = builder.create();
+        settingsDialog.show();
+        settingsDialog.getWindow().setDimAmount(0);
+        settingsDialog.getWindow().setLayout(420, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        settingsDialog.getWindow().setGravity(Gravity.TOP | Gravity.RIGHT);
+        settingsDialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.settings_dialog_background));
+    }
+
+    //----------------Button BookedTrips----------------//
+    public void SelectBookedTrips(View v){
+
+    }
+
+    //---------------Button OfferTrips---------------//
+    public void SelectOfferedTrips(View v){
+
+    }
+
+    //-------------Button Get A Ride----------------//
+    public void SelectGetARide(View v){
+
+    }
+
+    //------------Button Offer A Ride--------------//
+    public void SelectOfferARide(View v){
+
+    }
+
+    //---------------Back Button-------------------//
+    public void BackArrow(View v){
+        onBackPressed();
+    }
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        return;
+    }
+
+
 }
