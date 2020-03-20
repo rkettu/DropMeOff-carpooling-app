e090663349fd9a627528c951037060dc03082a1f
Saku Jamsa <t8jasa01@students.oamk.fi>
2020-03-20 14:00:38 +0200

    turha maps_activity poistettu. valmis mergeen

--- app/src/main/java/com/example/mobproj2020new/MainActivity.java
+++ app/src/main/java/com/example/mobproj2020new/MainActivity.java
@@ -13,17 +13,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.startButton).setOnClickListener(this);
+
         findViewById(R.id.startButton2).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.startButton) {
-            Intent maps_testi = new Intent(this, MapsActivity.class);
-            startActivity(maps_testi);
-        }
-        else if (v.getId() == R.id.startButton2){
+        if (v.getId() == R.id.startButton2){
             Intent route = new Intent(this, RouteActivity.class);
             startActivity(route);
         }
