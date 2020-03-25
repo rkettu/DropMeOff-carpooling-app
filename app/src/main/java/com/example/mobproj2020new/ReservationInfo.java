3eb3c2aae0acfa312a5cc13b2d802cd90c6b954c
Samu <t8masa00@students.oamk.fi>
2020-03-25 02:15:20 +0200

    Matkojen tarkemmat tiedot sivu UI

---
+++ app/src/main/java/com/example/mobproj2020new/ReservationInfo.java
@@ -0,0 +1,13 @@
+package com.example.mobproj2020new;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReservationInfo extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        setContentView(R.layout.reservation_info);
+    }
+}
